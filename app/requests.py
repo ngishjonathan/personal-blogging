48bff46b9c2e9a7af1179b7d4c5fd687735b50d7
ngishjonathan <balozimwash@gmil.com>
2019-06-03 18:00:15 +0300

    epoyment

--- app/requests.py
+++ app/requests.py
@@ -1,8 +1,1 @@
-# import requests
 
-
-# def random_post():
-        # url = 'http://quotes.stormconsultancy.co.uk/random.json'
-       #  pos = requests.get(url)
-       #  posts = pos.json()
-       # return posts
